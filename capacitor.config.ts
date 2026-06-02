96a972f04d90e4c1570ddd95c55b6a6f04f5052f
wilmer Paez <wpaezgil@gmail.com>
2026-06-01 22:52:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppApi',
+  webDir: 'www'
+};
+
+export default config;
